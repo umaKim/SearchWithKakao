6824ceeeafbe98c284ba8eea6c54513d8a6fef62
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-10-20 17:56:32 +0900

    chore: initial set up

---
+++ SearchWithKakaoApi/SearchWithKakaoApiUITests/SearchWithKakaoApiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SearchWithKakaoApiUITestsLaunchTests.swift
+//  SearchWithKakaoApiUITests
+//
+//  Created by 김윤석 on 2022/10/20.
+//
+
+import XCTest
+
+final class SearchWithKakaoApiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
