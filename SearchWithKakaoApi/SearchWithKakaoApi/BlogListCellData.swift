65441a70e62f30b08c3f3a11ba0dd8c1a2f67dbb
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-10-22 21:57:00 +0900

    chore: declare BlogListCellData struct

---
+++ SearchWithKakaoApi/SearchWithKakaoApi/BlogListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  BlogListCellData.swift
+//  SearchWithKakaoApi
+//
+//  Created by 김윤석 on 2022/10/20.
+//
+
+import Foundation
+
+struct BlogListCellData {
+    let thumbnailUrl: URL?
+    let name: String?
+    let title: String?
+    let datetime: Date?
+}
